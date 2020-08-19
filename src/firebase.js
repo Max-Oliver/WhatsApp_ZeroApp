7e65eb1f5a393600cd0bf51be63cc0126b74455e
Maximiliano Nicolas Olivero <maximiliano.olivero@globant.com>
2020-08-18 22:06:10 -0300

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnvZgRXatg3z6cKVmbJgUfTbDATfllM1c",
+    authDomain: "whatsappclone-17153.firebaseapp.com",
+    databaseURL: "https://whatsappclone-17153.firebaseio.com",
+    projectId: "whatsappclone-17153",
+    storageBucket: "whatsappclone-17153.appspot.com",
+    messagingSenderId: "343664633135",
+    appId: "1:343664633135:web:917760f26cfd529a02edda",
+    measurementId: "G-KDENVQJR7S"
+  };
